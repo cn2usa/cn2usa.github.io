84e48f27f0d857ed122cdd4ef0e64838ccd60218
cn2usa <ux25@outlook.com>
2024-11-14 14:04:14 -0800

    更新租房部分

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
-
+    staticDirectories: ['static'],
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'cn2usa', // Usually your GitHub org/user name.
